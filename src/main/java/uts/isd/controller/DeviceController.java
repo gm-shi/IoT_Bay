f86fdbdc2c59adddd4f5341893e176a9036f6d32
Littlenewww12138 <littlenewww@outlook.com>
2022-05-10 22:36:53 +1000

    update

--- src/main/java/uts/isd/controller/DeviceController.java
+++ src/main/java/uts/isd/controller/DeviceController.java
@@ -1,9 +1,29 @@
 package uts.isd.controller;
 
-import javax.servlet.annotation.*;
+import uts.isd.model.User;
+import uts.isd.model.UserAccessLog;
+import uts.isd.model.dao.UserAccessLogManager;
+import uts.isd.model.dao.UserManager;
+import uts.isd.model.dao.DeviceManager;
+import uts.isd.utils.DB;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 @WebServlet("/DeviceController")
 public class DeviceController {
+    DB db;
+    DeviceManager dc_manager;
+    String view = "deviceCollection.jsp";
+    public DeviceController() throws SQLException{
+        super();
+        db = new DB();
+        dc_manager = new DeviceManager(db);
+    }
+
 
 
 }
--- src/main/java/uts/isd/model/dao/DeviceManager.java
+++ src/main/java/uts/isd/model/dao/DeviceManager.java
@@ -4,6 +4,7 @@ import uts.isd.model.Device;
 import uts.isd.utils.DB;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DeviceManager {
     private DB db;
@@ -14,5 +15,21 @@ public class DeviceManager {
     private Connection conn() throws SQLException {
         return db.connection();
     }
+    public int create(Device device) throws SQLException{
+        String sqlQuery = "INSERT INTO item" + "(item_id, item_name, item_location, item_price, vendor_id, item_description, cate_id, item_quantity)" + "VALUES(?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement statement = conn().prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+        statement.setInt(1, device.getItemId());
+        statement.setString(2, device.getItemName());
+        statement.setString(3, device.getItemLocation());
+        statement.setDouble(4, device.getItemPrice());
+        statement.setString(5, device.getVendorId());
+        statement.setString(6, device.getItemDescription());
+        statement.setInt(7, device.getCateId());
+        statement.setInt(8, device.getItemQuantity());
 
+        statement.executeUpdate();
+        return 0;
+
+
+    }
 }
